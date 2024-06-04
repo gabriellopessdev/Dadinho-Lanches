b851e53f33c959b1b9c34bd955fcf81cce39af5c
Gabriellopessdev <gabriellopessdev@gmail.com>
2024-06-04 09:32:54 -0300

    update

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,60 +1,29 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css'
 import MealsItem  from './MealsItem/MealsItem';
 
 const AvaibleMeals = () => {
-  const [meals, setMeals] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [httpError, setHttpError] = useState();
-
-  useEffect(() => {
-    const fetchMeals = async() => {
-    const response = await fetch('https://projeto-novo-1552130917101-default-rtdb.firebaseio.com/meals.json');
-
-    if(!response.ok) {
-      throw new Error('Something went wrong');
-    }
-
-    const responseData = await response.json();
-
-    const loadedMeals = [];
-
-    for (const key in responseData) {
-      loadedMeals.push({
-        id: key,
-        name: responseData[key].name,
-        description: responseData[key].description,
-        price: responseData[key].price,
-      });
-    }
-
-    setMeals(loadedMeals);
-    setIsLoading(false);
-    }
-
-    fetchMeals().catch(error => {
-        setIsLoading(false);
-        setHttpError(error.message)
-    });
-  }, [])
-
-  if (isLoading) {
-    return (
-      <section className={classes.MealsLoading}>
-        <p>Leading...</p>
-      </section>
-    )
-  }
-
-  if (httpError) {
-    return (
-      <section className={classes.MealsError}>
-        <p>{httpError}</p>
-    </section>
-    )
-  }
-  
+  const [meals] = useState([
+    {
+      id: 'meal1',
+      name: 'Misto',
+      description: 'Pão, 2 Presunto, 2 mussarela, batata palha e milho',
+      price: 10.00,
+    },
+    {
+      id: 'meal1',
+      name: 'Misto',
+      description: 'Pão, 2 Presunto, 2 mussarela, batata palha e milho',
+      price: 10.00,
+    },
+    {
+      id: 'meal1',
+      name: 'Misto',
+      description: 'Pão, 2 Presunto, 2 mussarela, batata palha e milho',
+      price: 10.00,
+    },
+  ]);
     const mealList = meals.map(meal => (
       <MealsItem
         id={meal.id}
